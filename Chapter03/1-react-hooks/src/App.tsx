9e5ec37e0e0df6ae3da6e810f7f64acb2f54da5d
praiseolotu <olioxxayo@gmail.com>
2021-09-09 20:37:45 +0100

    updated package.json

--- Chapter03/1-react-hooks/src/App.tsx
+++ Chapter03/1-react-hooks/src/App.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import Issues2 from './Issues'
+import Counter from './Counter'
 
 const App = () => {
   return (
+    <>
     <Issues2 />
+    <Counter/>
+    </>
   )
 }
 
